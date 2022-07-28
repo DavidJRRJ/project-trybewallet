53bd98c31e4de9983a9209fe563e93a4408e46d1
David <david.lourenco94@gmail.com>
2022-07-28 17:51:23 -0300

    Primeiro Requisito

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,106 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { actionEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      // buttonOff: true,
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    console.log(this.state);
+    this.verifyPassword();
+    this.verifyEmail();
+  };
+
+  handleClick = () => {
+    const { history, globalMail } = this.props;
+    const { email } = this.state;
+    globalMail(email);
+    history.push('/carteira');
+  };
+
+  verifyPassword = () => {
+    const { password } = this.state;
+    const passwordLength = 6;
+    console.log(password);
+    if (password.length >= passwordLength) {
+      // this.setState({
+      //   buttonOff: false,
+      // });
+      return true;
+    }
+    return false;
+  };
+
+  verifyEmail = () => {
+    const { email } = this.state;
+    const emailRegex = /[a-zA-Z0-9]+[.]?([a-zA-Z0-9]+)?[@][a-z]{3,9}[.][a-z]{2,5}/g;
+    const emailCheck = emailRegex.test(email);
+    if (emailCheck === true) {
+      // this.setState({
+      //   buttonOff: false,
+      // });
+      return true;
+    }
+    return false;
+  };
+
   render() {
-    return <div>Login</div>;
+    // const { buttonOff } = this.state;
+    return (
+      <div>
+        <label htmlFor="email">
+          Email:
+          <input
+            name="email"
+            type="email"
+            data-testid="email-input"
+            onChange={ this.handleChange }
+            required
+          />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input
+            name="password"
+            type="password"
+            data-testid="password-input"
+            onChange={ this.handleChange }
+            required
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={ this.handleClick }
+          disabled={ !(this.verifyEmail() && this.verifyPassword()) }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  globalMail: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  globalMail: (email) => dispatch(actionEmail(email)),
+});
+
+export default connect(null, mapDispatchToProps)(Login);
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,1 +1,11 @@
-// Coloque aqui suas actions
+import { USER_EMAIL } from './actionTypes';
+
+export const actionEmail = (email) => ({
+  type: USER_EMAIL,
+  email,
+});
+
+export const actionWallet = (wallet) => ({
+  type: 'WALLET',
+  wallet,
+});
